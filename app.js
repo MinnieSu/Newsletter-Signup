dd3bce4525b9e347694d2a0b3c376af6ac20b946
Minnis <minnis@Minniss-MacBook-Air.local>
2023-04-05 21:15:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: "70b9927f7d209dd9d8ba12da3489a9a8-us21",
+  server: "us21",
+});
+
+// enable our server to serve up static file.
+app.use(express.static("public"));
+
+//enable express to parse URL-encoded body i.e. info from HTML form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set up the get route to the sign up page and test it on the browser at localhost:3000
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.emailAddress;
+
+  // post data to Mailchimp server
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+  async function run() {
+    try {
+      const response = await mailchimp.lists.addListMember("df9eacda0a", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
+        },
+      });
+
+      console.log(
+        `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+      );
+
+      res.sendFile(__dirname + "/success.html");
+    } catch (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  }
+  run();
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server is running on port 3000.");
+});
+
+// API Key
+// 70b9927f7d209dd9d8ba12da3489a9a8-us21
+
+// List id: helps mailchimp to identify the list that you want to put your subscribers into
+// df9eacda0a
